70269fcaa35dbcb94b21d074cf65f94e986b1ffa
Muhammad Rizal <muhammadrizal1702199@gmail.com>
2024-12-07 22:31:39 +0700

    membuat form data_mitra di data_mitra.php

---
+++ view/superadmin/data_mitra.php
@@ -0,0 +1,61 @@
+<div class="container">
+    <h2 class="text-center">Form Data Mitra</h2>
+    <form action="" method="">
+            <!-- nama instansi -->
+            <div class="mb-3">
+                    <label for="nama-instansi">Nama Instansi</label>
+                    <input type="text" name="nama-instansi" id="nama-instansi" class="from-control" required>
+            </div>
+
+            <!-- email -->
+            <div class="mb-3">
+                    <label for="email-instansi">Email Instansi</label>
+                    <input type="email" name="email-instansi" id="email-instansi" class="from-control" required>
+            </div>
+
+            <!-- bidang usaha -->
+            <div class="mb-3">
+                    <label for="bidang-usaha">Bidang Usaha</label>
+                    <input type="text" name="bidang-usaha" id="bidang-usaha" class="from-control" required>
+            </div>
+
+            <!-- nomor-telphone -->
+            <div class="mb-3">
+                    <label for="no-tlp">Nomor Telphone</label>
+                    <input type="tel" name="no-tlp" id="no-tlp" class="from-control" required>
+            </div>
+
+            <!-- alamat -->
+            <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">Alamat Instansi</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="alamat-instansi"></textarea>
+            </div>
+
+            <!-- kota -->
+            <div class="mb-3">
+                    <label for="kota">Kota</label>
+                    <input type="text" name="kota" id="kota" class="from-control" required>
+            </div>
+
+            <!-- provinsi -->
+             <div class="mb-3">
+                    <label for="provinsi">Provinsi</label>
+                    <input type="text" name="provinsi" id="provinsi" class="from-control" required>
+            </div>
+
+            <!-- negara -->
+             <div class="mb-3">
+                    <label for="negara">Negara</label>
+                    <input type="text" name="negara" id="negara" class="from-control" >
+            </div>
+
+            <!-- webiste -->
+             <div class="mb-3">
+                    <label for="website">Website</label>
+                    <input type="text" name="website" id="website" class="from-control" >
+            </div>
+            
+             <!-- submit -->
+             <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
